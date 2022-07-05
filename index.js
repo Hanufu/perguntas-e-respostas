57bf90ebe56a9fce7fc4bc015bacda8639daabc7
Eduardo Ribeiro da Silva <eduardo20003131@gmail.com>
2022-07-04 23:10:34 -0300

    adicionando EJS e config Inicial

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 const express = require("express");
 const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get("/:nome/:lang", (req, res) =>{
+    
+    const nome = req.params.nome;
+    const lang = req.params.lang;
+
+    res.render("index",{
+        nome: nome,
+        lang: lang,
+        empresa: "Group Inovale",
+        inscritos: 4000
+    });
+})
+
+app.listen(8080, ()=>{
+    console.log("App rodando!");
+});
