9da29c980d94c55d9836579de82b4bd74dad989e
Eduardo Ribeiro <eduardo20003131@gmail.com>
2022-07-07 14:13:08 -0300

    Instalando o bodyParser

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const app = express();
-
+const bodyParser = require("body-parser");
 //setando engine, e arquivos estaticos
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //rotas
 app.get("/", (req, res)=>{
     res.render("index")
 });
+
 app.get("/perguntar", (req, res)=>{
     res.render('perguntar')
 });
+
+app.post("/salvarpergunta",(req, res)=>{
+	res.send("Formulário recebido");
+});
+
 
 //criando servidor
 app.listen(8080, ()=>{
