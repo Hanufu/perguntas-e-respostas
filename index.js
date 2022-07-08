a5f7e6acced121637c176138a9e2bf1120b57230
Eduardo Ribeiro <eduardo20003131@gmail.com>
2022-07-08 09:43:16 -0300

    Adicionando dependencias do sequelize
    Criamos nosso banco de dados, fizemos as configurações iniciais, e nossa primeira conexão

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const connection = require("./database/database");
+
+//database
+connection.authenticate().then(() =>{
+    console.log("Conexão feita com o banco de dados!");
+}).catch((msgErro)=>{
+    console.log(msgErro);
+})
+
 //setando engine, e arquivos estaticos
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
